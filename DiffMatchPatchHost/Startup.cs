c81e2081d10edad2217dac1818a26b576dc404d4
Abrar Jahin <abrarjahin@live.com>
2018-04-16 16:50:18 +0600

    Use header forworder

--- DiffMatchPatchHost/Startup.cs
+++ DiffMatchPatchHost/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -27,6 +28,12 @@ namespace DiffMatchPatchHost
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            //Configure the middleware to forward the X-Forwarded-For and X-Forwarded-Proto headers
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseBrowserLink();
