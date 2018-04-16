04d4f26c8d75cf626038d33c5fbff05110639b7c
Abrar Jahin <abrarjahin@live.com>
2018-04-16 13:17:40 +0600

    Add project files.

---
+++ DiffMatchPatchHost/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DiffMatchPatchHost.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
